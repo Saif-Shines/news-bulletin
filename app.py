d45b61c4b918e7b9b5b8793353707822a8b467dc
Saif Ali Shaik <saifshine7@gmail.com>
2023-06-14 21:12:53 +0530

    🚀 RELEASE: fetch top 10 articles

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ config = dotenv_values(".env")
 
 news_api_key = config["NEWS_API_KEY"]
 query = {
-    "q": "bard ai"
+    "q": sys.argv[1],
 }
 endpoint = "https://newsapi.org/v2/everything"
 headers = {
@@ -14,6 +14,14 @@ headers = {
 }
 
 if __name__ == '__main__':
-    print(f"Starting...")
+    print(f"Fetching news")
     news_feed = requests.get(endpoint, headers=headers, params=query)
-    print(f"Newsfeed {news_feed.json()}")
+    news_feed = news_feed.json()
+    posts = news_feed["articles"]
+    count = 0
+    for post in posts:
+        count += 1
+        print("ℹ", post["title"], "\n🔗", post["url"], "\n---")
+        if count == 10:
+            print("✅ Enjoy reading top 10 articles")
+            break
